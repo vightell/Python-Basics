18087223e9d4ad8254f6437e68d6ce5cc6c2e466
Vightell <vightellr@email.com>
2026-04-29 14:41:27 +0300

    I was teasing ChatGPT plus I combined list, loop and if in one.

---
+++ day10.py
@@ -0,0 +1,8 @@
+zoo=["lions","pigs","chatgpt models","ants","foxes","gorillas"]
+for animal in zoo:
+ if len(animal)>6: 
+  print(animal,"are valuble")
+ elif len(animal)<5:
+  print(animal,"maybe will get a chance")   
+ else:
+  print(animal,"are brainless")   
