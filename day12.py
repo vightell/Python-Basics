37d2057cba02a7031709f663d2124a2216537165
Vightell <vightellr@email.com>
2026-05-04 23:54:03 +0300

    Today was easy, it's was abt input, and I finished it fast, props to me :)

---
+++ day12.py
@@ -0,0 +1,13 @@
+food=input("What Do you Wanna Eat?")
+drink=input("And What Do You Wanna Drink?")
+if food==("Apple"):
+ print("Here, Take it.")
+else: 
+ print("Nah I don't Have That, Let's Go get it.")   
+
+if drink==("Water"):
+ print("You already drink A lot, That's why you cry a lot LOLpwd.")
+elif drink==("Milk"):
+ print("I LOVE milk, Here it is!")    
+else:
+ print("Nah hydrate first.")       
