566d0edc0e8ed51d5de983d3785e222efaae01ab
Vightell <vightellr@email.com>
2026-05-16 20:06:46 +0300

    I found Day2 if else old Artifact.

---
+++ day.3.py
@@ -0,0 +1,17 @@
+jason="smart"
+if jason=="broke":
+ laugh="lol"
+ print(laugh, "I'm sorry")
+ print ("BOHAHAHA")
+ input("mock")
+elif jason=="smart":
+ print ("meh")
+ input("only meet for homework lol")
+elif jason=="lazy":
+ print ("ugh me too")
+ input("shared trauwma")
+else:
+ damn="Idk how to react"
+ print(damn, "wish ya a good life")
+ print("Oh")
+ input("friends")
