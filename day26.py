fe2d61ace1fd7863c74e7f70218334fa570099d5
Vightell <vightellr@email.com>
2026-05-17 14:35:33 +0300

    I CONNECTED THIS DAMN BOT TO MY SCRAPER, I LOST SOME BRAIN CELLS IN THE PROCESS.

---
+++ day26.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+TOKEN = "8927093512:AAH0imLLS9W2nVhG9BjM6tFndMpC9-Qt1s0"
+current_index = 0
+
+def get_news():
+    global current_index
+    
+    response = requests.get("https://news.ycombinator.com/")
+    soup = BeautifulSoup(response.content, "html.parser")
+    titles = soup.select(".titleline > a")
+    news = []
+
+    start = current_index
+    end = current_index + 5
+    current_index += 5
+
+    for item in titles:
+        news.append({
+            "title": item.get_text(),
+            "link": item["href"]
+        })
+    return news[start:end]        
+
+async def hi(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        f"Hi {update.effective_user.first_name}"
+    )
+
+async def news(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Recently, blah blah blah..."
+    )
+
+async def chaos(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Shall we summon the Internet Chaos Or Scan the Digital Abyss?"
+    )
+
+async def abyss(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    articles = get_news()
+    message=""
+    for article in articles:
+        message +=(
+        f"• {article['title']}\n"
+        f"  Link: {article['link']}\n\n"
+        )
+    if articles:
+        await update.message.reply_text(message)
+    else:
+        await update.message.reply_text("The Digital Abyss is silent... No news found.")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("hi", hi))
+app.add_handler(CommandHandler("news", news))
+app.add_handler(CommandHandler("chaos", chaos))
+app.add_handler(CommandHandler("abyss", abyss))
+print("Chaos Scraper is sprinting...")
+app.run_polling()
